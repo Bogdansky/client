71de8c98d77a60e4fb3443b9d1a6d2d5f915a400
Bogdan <bogdya2@gmail.com>
2019-05-12 12:07:55 +0300

    Add error component

---
+++ src/components/Error.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Error.css'
+
+export class Error extends React.Component {
+    static displayName = Error.name;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            statusCode: localStorage.getItem("statusCode"),
+            message: localStorage.getItem("message")
+        };
+
+    }
+
+    render() {
+        return (
+            <div className="errorBody">
+                <div id="titles">
+                    <div id="titlecontent">
+                        <p>Ошибка {this.state.statusCode}</p>
+                        <p>{this.state.message}</p>
+                    </div>
+                </div>
+            </div>  
+        );
+    }
+}
