5fd43252bb41c6f80ce5dd465c0ccd1ed2d1b188
Bogdan <bogdya2@gmail.com>
2019-05-26 15:52:46 +0300

    Add function for pushing new book

--- src/components/Library.js
+++ src/components/Library.js
@@ -2,6 +2,7 @@ import React from 'react'
 import $ from 'jquery'
 import { Route, Redirect } from 'react-router'
 import Book from './Book'
+import AddBook from './AddBook'
 import './Library.css'
 
 export default class Library extends React.Component {
@@ -15,6 +16,7 @@ export default class Library extends React.Component {
         }
 
         this.changeState = this.changeState.bind(this);
+        this.add = this.add.bind(this);
 
         fetch('https://localhost:44326/api/books')
             .then(response => response.json())
@@ -35,6 +37,12 @@ export default class Library extends React.Component {
         }
     }
 
+    add(book) {
+        let books = this.state.books;
+        books.push(book);
+        this.setState({ books: books });
+    }
+
     render() {
         return (
             <div className="container h-100">
@@ -45,51 +53,17 @@ export default class Library extends React.Component {
                     </div>
                 </div>
                 <div className="row">
-                    <div className="row" id="books" style={{ display: 'line', overflow: 'visible', scrollbarColor: 'black' }}>
+                    <div className="row" id="books" style={{ display: 'line' }}>
                         {this.state.books.map((b, index) => {
                             return <Book key={b.id} {...b} />
                         })}
                     </div>
-                    <div className="row justify-content-center align-items-center" id="add-book" style={{ display: 'none'}}>
-                        <form className="col-12">
-                            <div className="form-group">
-                                <label htmlFor="inputName">Name</label>
-                                <input type="text" className="form-control" id="inputName" placeholder="Name"/>
-                                <small id="emailHelp" className="form-text text-muted">Enter name of book there</small>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="inputAuthor">Author</label>
-                                <input type="text" className="form-control" id="inputAuthor" placeholder="Name" />
-                                <small id="emailHelp" className="form-text text-muted">Enter author of book there</small>
-                            </div>
-                            <div className="form-group">
-                                <div>
-                                    <label>Cover</label>
-                                </div>
-                                <div className="form-check form-check-inline">
-                                    <input type="checkbox" className="form-control" id="byUpload" />
-                                    <label className="form-check-label">Upload file</label>
-                                </div>
-                                <div className="form-group" id="byUrl">
-                                    <input type="url" className="form-control" id="inputCover" placeholder="https://example.com/1.jpg" />
-                                    <small className="form-text text-muted">Enter the url of the picture</small>
-                                </div>
-                                <div className="form-group" id="byUpload" style={{ display: 'none' }}>
-                                    <input type="file" className="form-control-file" id="uploadFile" />
-                                    <small className="form-text text-muted"></small>
-                                </div>
-                                <input type="text" className="form-control" id="inputCover" placeholder="Name" />
-                                <small id="emailHelp" className="form-text text-muted">Enter name of book there</small>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="inputNumber">Number of pages</label>
-                                <input type="number" min="1" className="form-control" id="inputNumber" placeholder="111" />
-                            </div>
-                            <button type="submit" className="btn btn-primary">Submit</button>
-                        </form>
+                    <div className="row" id="add-book" style={{display: 'none'}}>
+                        <AddBook add={this.add}/>
                     </div>
                 </div>
+                    <button></button>
             </div>
-                        )
-                }
+            )
+    }
 }
