cdbba69ca44abaf8af1a94a1f0c1e1b0436dfadb
Bogdan <bogdya2@gmail.com>
2019-06-02 13:54:45 +0300

    Resolve error with getting user info after registration

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -48,7 +48,7 @@ export class SignUp extends React.Component {
                     if (data.token) {
                         localStorage.setItem("userId", data.id);
                         localStorage.setItem("token", data.token);
-                        localStorage.setItem("userinfo", data.userInfo);
+                        localStorage.setItem("userinfo", JSON.stringify(data.userInfo));
                         this.setState({ logged: true })
 
                     } else if (data.statusCode && data.message) {
